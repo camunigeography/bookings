5cfe6752d5e72a075bdd3c0cb033d1597dd80cc2
mvl22 <martin@lucas-smith.co.uk>
2015-03-02 12:31:38 +0000

    Code changes dated 23/Jul/2013; changes to the form to add per-section booking for different departments and move some settings into settings infrastructure

--- bookings.php
+++ bookings.php
@@ -20,6 +20,7 @@ class bookings extends frontControllerApplication
 			'table' => 'bookings',
 			'administrators' => true,
 			'settings' => true,
+			'settingsTableExplodeTextarea' => true,
 			'serverAdministrator'	=> NULL,	// E-mail address of the server administrator
 			'recipient'	=> NULL,	// Who to send the e-mail requests to
 			'form' => true,
@@ -70,7 +71,7 @@ class bookings extends frontControllerApplication
 		
 		# Process the place titles
 		if ($this->action != 'settings') {
-			$placeTitles = explode ("\n", $this->settings['placeTitles']);
+			$placeTitles = $this->settings['placeTitles'];	// Cache the setting
 			$this->settings['placeTitles'] = array ();
 			foreach ($placeTitles as $index => $placeTitle) {
 				$place = $index + 1;	// Indexed from 1; the numeric value is what is stored, not the label
@@ -81,24 +82,44 @@ class bookings extends frontControllerApplication
 		# Get the dates
 		$this->dates = $this->getDates ();
 		
-		# Get the data or end
-		if (!$this->data = $this->getData ()) {return false;}
+		# Get the data (which may be empty)
+		$this->data = $this->getData ();
 		
+	}
+	
+	
+	# Settings additional processing
+	public function settingsGetUnfinalised (&$form)
+	{
+		# Add getUnfinalised processing
+		if ($unfinalisedData = $form->getUnfinalisedData ()) {
+			
+			# Check that the number of input widgets in the eventual editing page doesn't exceed the PHP max_input_vars setting
+			$submittedPlaceTitles = explode ("\n", trim ($this->settings['placeTitles']));
+			$widgetsRequired = ($unfinalisedData['listMonthsAheadPrivate'] * 31 * count ($submittedPlaceTitles));
+			if ($widgetsRequired > ini_get ('max_input_vars')) {
+				$form->registerProblem ('toomany', 'The number of months is too high (too many input boxes would have to be created, beyond what the server can handle). Please reduce the number.', 'listMonthsAheadPrivate');
+			}
+		}
 	}
 	
 	
 	# Function to get the dates for future months
 	public function getDates ()
 	{
+		# Determine the weekend days to use
+		$weekdays = ($this->settings['weekdays'] ? explode (',', strtolower ($this->settings['weekdays'])) : array ());
+		
 		# Create an array of dates in future months
-		$dates = timedate::getDatesForFutureMonths ($this->settings['listMonthsAheadPublic'], 'Y-m-d', $removeWeekends = true);
+		$dates = timedate::getDatesForFutureMonths ($this->settings['listMonthsAheadPublic'], 'Y-m-d', $weekdays);
 		
 		# If the user is an admin, show the fuller list, and determine the first date that is private
 		if ($this->userIsAdministrator) {
 			$datesPublic = $dates;
-			$dates = timedate::getDatesForFutureMonths ($this->settings['listMonthsAheadPrivate'], 'Y-m-d', $removeWeekends = true);
+			$dates = timedate::getDatesForFutureMonths ($this->settings['listMonthsAheadPrivate'], 'Y-m-d', $weekdays);
 			$privateDates = array_diff ($dates, $datesPublic);
-			$this->firstPrivateDate = reset (array_values ($privateDates));
+			$privateDatesValues = array_values ($privateDates);	// This temp has to be used to avoid "Strict Standards: Only variables should be passed by reference"
+			$this->firstPrivateDate = reset ($privateDatesValues);
 		}
 		
 		# Remove earliest dates
@@ -116,7 +137,8 @@ class bookings extends frontControllerApplication
 	{
 		# Determine the first and last dates, so that only this range is obtained for efficiency
 		$firstDate = $this->dates[0];
-		$untilDate = end (array_values ($this->dates));
+		$datesValues = array_values ($this->dates);	// This temp has to be used to avoid "Strict Standards: Only variables should be passed by reference"
+		$untilDate = end ($datesValues);
 		
 		# Get any data for between these ranges
 		$query = "SELECT
@@ -227,7 +249,7 @@ class bookings extends frontControllerApplication
 				}
 				
 				# Insert confirmation into the start of the page
-				$confirmationHtml = "\n<div class=\"graybox\">\n\t<p class=\"success\"><img src=\"/images/icons/tick.png\" alt=\"Tick\" class=\"icon\" /> <strong>The " . (count ($changedFields) == 1 ? 'change has' : 'changes have') . " now been made, as shown below. <a href=\"{$this->baseUrl}/\">Return to public listing</a></strong> or edit further below.</p>\n</div>";
+				$confirmationHtml = "\n<div class=\"graybox\">\n\t<p class=\"success\"><img src=\"/images/icons/tick.png\" alt=\"Tick\" class=\"icon\" /> <strong>The " . (count ($changedFields) == 1 ? 'change has' : 'changes have') . " now been made, as shown below. <a href=\"{$this->baseUrl}/\">Return to the public listing</a></strong> or edit further below.</p>\n</div>";
 				$highlightIds = array ();
 				foreach ($changedFields as $field) {
 					$highlightIds[] = '#form_' . $field;
@@ -336,77 +358,361 @@ class bookings extends frontControllerApplication
 		# Start a form
 		$form = new form (array (
 			'displayRestrictions' => false,
-			'displayDescriptions' => true,
+			'databaseConnection' => $this->databaseConnection,
 			'formCompleteText' => false,
 			'antispam' => true,
 			'div' => 'lines horizontalonly bookingform',
 			'autofocus' => true,
+			'cols' => 40,
+			'nullText' => false,
 		));
 		
 		# Determine the form fields
 		$form->heading ('p', application::htmlUl (array ("<a href=\"{$this->baseUrl}/\">Back to list of dates</a>")));
-		$form->heading ('', $this->settings['bookingPageTextHtml']);
+		if ($this->settings['bookingPageTextHtml']) {
+			$form->heading ('', $this->settings['bookingPageTextHtml']);
+		}
 		$form->heading (2, "Request a booking for: the <u>{$period}</u> of <u>{$dateString}</u>");
 		
-		$form->input (array (
-			'name'			=> 'name',
-			'title'			=> 'Your name',
-			'required'		=> true,
-		));
-		$form->input (array (
-			'name'			=> 'institution',
-			'title'			=> 'Your institution',
-			'description'	=> 'Please state the academic institution from which you are applying.',
-			'required'		=> true,
-		));
-		$form->textarea (array (
-			'name'			=> 'documents',
-			'title'			=> 'Which documents do you wish to see?',
-			'required'		=> true,
-			'cols'			=> 40,
-		));
-		$form->email (array (
-			'name'			=> 'email',
-			'title'			=> 'E-mail address',
-			'description'	=> 'Correspondence by e-mail is preferred where possible.',
-			'required'		=> false,
-		));
-		$form->textarea (array (
-			'name'			=> 'address',
-			'title'			=> 'Your address',
-			'required'		=> true,
-			'cols'			=> 40,
-			'rows'			=> 3,
-		));
-		$form->input (array (
-			'name'			=> 'phone',
-			'title'			=> 'Phone number',
-			'required'		=> false,
-		));
-		$form->input (array (
-			'name'			=> 'arrivaltime',
-			'title'			=> 'Arrival time on first day',
-			'required'		=> true,
-		));
-		$form->textarea (array (
-			'name'			=> 'subsequentdays',
-			'title'			=> 'Any subsequent (single/half-) days?',
-			'description'	=> "If you need to stay for more than just the {$period} of {$dateString}, please give full details here. You must specify specific single days or half-days only. Block bookings will not be accepted.",
-			'required'		=> false,
-			'rows'			=> 3,
+		# Set the table
+		$table = $this->settings['tablePrefix'] . 'requests';
+		
+		# Set the attributes
+		$attributes = array ();
+		switch ($table) {
+			case 'archives_requests':
+				$attributes['email'] = array ('description'	=> 'Correspondence by e-mail is preferred where possible.', );
+				$attributes['subsequentdays'] = array ('description'	=> "If you need to stay for more than just the {$period} of {$dateString}, please give full details here. You must specify specific single days or half-days only. Block bookings will not be accepted.", );
+				break;
+				
+			case 'education_requests':
+				$attributes['visitType'] = array ('heading' => array (3 => 'Type of visit'), 'type' => 'radiobuttons', 'values' => array (
+					'Polar Museum visit'	=> 'Museum visit - self-guided (run entirely by the teacher or group leader)',
+					'Polar Museum workshop'	=> 'Museum workshop (run by a member of museum staff); Thursdays/Fridays only',
+					'Polar Museum tour'		=> 'Museum tour (led by a member of museum staff) - costs £80',
+					'Polar Museum outreach'	=> 'Outreach - will attract a cost',
+					'Polar Museum'			=> 'Other (please give details)',
+				));
+				$attributes['institutionType'] = array ('heading' => array (3 => 'Details of group'), );
+				$attributes['date'] = array ('heading' => array (3 => 'Details of visit'), 'picker' => true, );
+				$attributes['previsit'] = array ('picker' => true, );
+				$attributes['country'] = array ('type' => 'select', 'values' => $this->getCountries (), );
+				break;
+		}
+		
+		# Databind the form
+		$form->dataBinding (array (
+			'database' => $this->settings['database'],
+			'table' => $table,
+			'intelligence' => true,
+			'attributes' => $attributes,
+			'exclude' => array ('internalAgeGroups', 'internalVisitContent', 'internalVisitContentOther', 'internalPhoneCallLog'),
+			'data' => array ('date' => $date, 'period' => ucfirst ($period), ),
 		));
 		
+		# Add constraints
+		if ($table == 'education_requests') {
+			if ($unfinalisedData = $form->getUnfinalisedData ()) {
+				
+				# Require school details
+				$requireSchoolInfo = array ('School', 'Language school', 'Further Education (sixth form)');
+				if (in_array ($unfinalisedData['institutionType'], $requireSchoolInfo) && (!strlen ($unfinalisedData['adults']))) {
+					$form->registerProblem ('schooladutsmissing', 'You need to enter the number of adults for school visits.', 'adults');
+				}
+				
+				# Require details if 'other' selected for type
+				if (($unfinalisedData['visitType'] == 'Polar Museum') && (!strlen ($unfinalisedData['visitTypeOther']))) {
+					$form->registerProblem ('visittypedetailsmissing', 'You need to give further details for the type of visit.', 'visitTypeOther');
+				}
+				if (($unfinalisedData['visitType'] != 'Polar Museum') && (strlen ($unfinalisedData['visitTypeOther']))) {
+					$form->registerProblem ('visittypedetailsadded', 'You gave further details for the type of visit but selected a different option.', 'visitTypeOther');
+				}
+			}
+		}
+		
+		# Data protection statement
+		$form->heading ('p', 'By submitting this form you are agreeing to this information being kept as a record of your visit.');
+		
 		# E-mail the result to the webmaster as a backup record
-		$form->setOutputEmail ($this->settings['recipient'], $this->settings['serverAdministrator'], $this->settings['applicationName'] . " for {$dateString} in the {$period}", NULL, $replyToField = 'email');
+		$subject = $this->settings['applicationName'] . " for {$dateString} in the {$period}";
+		if ($table == 'education_requests') {
+			$subject = "{name} - {visitType|compiled}";
+		}
+		$form->setOutputEmail ($this->settings['recipient'], $this->settings['serverAdministrator'], $subject, NULL, $replyToField = 'email');
 		
 		# Confirm what they submitted on screen
 		$form->setOutputScreen ();
 		
-		# Process the form and produce the results
-		$form->process ($html);
+		# Process the form
+		if ($result = $form->process ($html)) {
+			
+			# Save to the database
+			$this->databaseConnection->insert ($this->settings['database'], $table, $result);
+		}
 		
 		# Show the HTML
 		echo $html;
+	}
+	
+	
+	# Function to return a list of countries
+	private function getCountries ()
+	{
+		# Return the list
+		return array (
+			'United Kingdom',
+			'Group of visitors from multiple countries',
+			'---',
+			'Afghanistan',
+			'Aland Islands',
+			'Albania',
+			'Algeria',
+			'American Samoa',
+			'Andorra',
+			'Angola',
+			'Anguilla',
+			'Antarctica',
+			'Antigua and Barbuda',
+			'Argentina',
+			'Armenia',
+			'Aruba',
+			'Australia',
+			'Austria',
+			'Azerbaijan',
+			'Bahamas',
+			'Bahrain',
+			'Bangladesh',
+			'Barbados',
+			'Belarus',
+			'Belgium',
+			'Belize',
+			'Benin',
+			'Bermuda',
+			'Bhutan',
+			'Bolivia, Plurinational State of',
+			'Bonaire, Saint Eustatius and Saba',
+			'Bosnia and Herzegovina',
+			'Botswana',
+			'Bouvet Island',
+			'Brazil',
+			'British Indian Ocean Territory',
+			'Brunei Darussalam',
+			'Bulgaria',
+			'Burkina Faso',
+			'Burundi',
+			'Cambodia',
+			'Cameroon',
+			'Canada',
+			'Cape Verde',
+			'Cayman Islands',
+			'Central African Republic',
+			'Chad',
+			'Chile',
+			'China',
+			'Christmas Island',
+			'Cocos (Keeling) Islands',
+			'Colombia',
+			'Comoros',
+			'Congo',
+			'Congo, The Democratic Republic of the',
+			'Cook Islands',
+			'Costa Rica',
+			"Cote d'Ivoire",
+			'Croatia',
+			'Cuba',
+			'Curacao',
+			'Cyprus',
+			'Czech Republic',
+			'Denmark',
+			'Djibouti',
+			'Dominica',
+			'Dominican Republic',
+			'Ecuador',
+			'Egypt',
+			'El Salvador',
+			'Equatorial Guinea',
+			'Eritrea',
+			'Estonia',
+			'Ethiopia',
+			'Falkland Islands (Malvinas)',
+			'Faroe Islands',
+			'Fiji',
+			'Finland',
+			'France',
+			'French Guiana',
+			'French Polynesia',
+			'French Southern Territories',
+			'Gabon',
+			'Gambia',
+			'Georgia',
+			'Germany',
+			'Ghana',
+			'Gibraltar',
+			'Greece',
+			'Greenland',
+			'Grenada',
+			'Guadeloupe',
+			'Guam',
+			'Guatemala',
+			'Guernsey',
+			'Guinea',
+			'Guinea-Bissau',
+			'Guyana',
+			'Haiti',
+			'Heard Island and McDonald Islands',
+			'Holy See (Vatican City State)',
+			'Honduras',
+			'Hong Kong',
+			'Hungary',
+			'Iceland',
+			'India',
+			'Indonesia',
+			'Iran, Islamic Republic of',
+			'Iraq',
+			'Ireland',
+			'Isle of Man',
+			'Israel',
+			'Italy',
+			'Jamaica',
+			'Japan',
+			'Jersey',
+			'Jordan',
+			'Kazakhstan',
+			'Kenya',
+			'Kiribati',
+			"Korea, Democratic People's Republic of",
+			'Korea, Republic of',
+			'Kuwait',
+			'Kyrgyzstan',
+			"Lao People's Democratic Republic",
+			'Latvia',
+			'Lebanon',
+			'Lesotho',
+			'Liberia',
+			'Libyan Arab Jamahiriya',
+			'Liechtenstein',
+			'Lithuania',
+			'Luxembourg',
+			'Macao',
+			'Macedonia, The Former Yugoslav Republic of',
+			'Madagascar',
+			'Malawi',
+			'Malaysia',
+			'Maldives',
+			'Mali',
+			'Malta',
+			'Marshall Islands',
+			'Martinique',
+			'Mauritania',
+			'Mauritius',
+			'Mayotte',
+			'Mexico',
+			'Micronesia, Federated States of',
+			'Moldova, Republic of',
+			'Monaco',
+			'Mongolia',
+			'Montenegro',
+			'Montserrat',
+			'Morocco',
+			'Mozambique',
+			'Myanmar',
+			'Namibia',
+			'Nauru',
+			'Nepal',
+			'Netherlands',
+			'New Caledonia',
+			'New Zealand',
+			'Nicaragua',
+			'Niger',
+			'Nigeria',
+			'Niue',
+			'Norfolk Island',
+			'Northern Mariana Islands',
+			'Norway',
+			'Occupied Palestinian Territory',
+			'Oman',
+			'Pakistan',
+			'Palau',
+			'Panama',
+			'Papua New Guinea',
+			'Paraguay',
+			'Peru',
+			'Philippines',
+			'Pitcairn',
+			'Poland',
+			'Portugal',
+			'Puerto Rico',
+			'Qatar',
+			'Reunion',
+			'Romania',
+			'Russian Federation',
+			'Rwanda',
+			'Saint Barthelemy',
+			'Saint Helena, Ascension and Tristan da Cunha',
+			'Saint Kitts and Nevis',
+			'Saint Lucia',
+			'Saint Martin (French part)',
+			'Saint Pierre and Miquelon',
+			'Saint Vincent and The Grenadines',
+			'Samoa',
+			'San Marino',
+			'Sao Tome and Principe',
+			'Saudi Arabia',
+			'Senegal',
+			'Serbia',
+			'Seychelles',
+			'Sierra Leone',
+			'Singapore',
+			'Sint Maarten (Dutch part)',
+			'Slovakia',
+			'Slovenia',
+			'Solomon Islands',
+			'Somalia',
+			'South Africa',
+			'South Georgia and the South Sandwich Islands',
+			'Spain',
+			'Sri Lanka',
+			'North Sudan',
+			'South Sudan',
+			'Suriname',
+			'Svalbard and Jan Mayen',
+			'Swaziland',
+			'Sweden',
+			'Switzerland',
+			'Syrian Arab Republic',
+			'Taiwan, Province of China',
+			'Tajikistan',
+			'Tanzania, United Republic of',
+			'Thailand',
+			'Timor-Leste',
+			'Togo',
+			'Tokelau',
+			'Tonga',
+			'Trinidad and Tobago',
+			'Tunisia',
+			'Turkey',
+			'Turkmenistan',
+			'Turks and Caicos Islands',
+			'Tuvalu',
+			'Uganda',
+			'Ukraine',
+			'United Arab Emirates',
+			'United Kingdom (UK)',
+			'United States of America (USA)',
+			'United States Minor Outlying Islands',
+			'Uruguay',
+			'Uzbekistan',
+			'Vanuatu',
+			'Venezuela, Bolivarian Republic of',
+			'Viet Nam',
+			'Virgin Islands, British',
+			'Virgin Islands, U.S.',
+			'Wallis and Futuna',
+			'Western Sahara',
+			'Yemen',
+			'Zambia',
+			'Zimbabwe',
+		);
 	}
 }
 
